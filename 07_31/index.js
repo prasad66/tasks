5794d609fb84f35b0d382a72f6df8e2bd97881d7
prasad66 <54799384+prasad66@users.noreply.github.com>
2021-08-01 10:49:06 +0530

    Update index.js

--- 07_31/index.js
+++ 07_31/index.js
@@ -6,6 +6,6 @@ const stu=[
     {name:"Kavi" , gender:"female"}
     ];
     console.log('Total students =>',stu);
-    console.log("Female Students=>',stu.filter( student => student.gender === "female").map(stu => stu.name))
+    console.log("Female Students=>",stu.filter( student => student.gender === "female").map(stu => stu.name))
     console.log("Students name starting with 'A' =>",stu.filter( student => student["name"].startsWith("A")).map(stu => stu.name))
     console.log("Male students =>",stu.filter( student => student.gender === "male").length)
